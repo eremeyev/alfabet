dae877e7e65d821b16d526be56e12839b9d49a07
Andrey Eremeev <andrey.eremeyev@gmail.com>
2018-12-13 14:55:46 +0400

    delete italic

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require "colorize"
 commands = ("оплпол"*100).split("")
 
 alfabet30 = "АБВГДЕЁЖЗИКЛМНОПРСТУФХЧЦШЩЫЭЮЯ".split("")
-res = alfabet30.map{|l| l + commands[rand(300)].red.italic}
+res = alfabet30.map{|l| l + commands[rand(300)].red}
 
 # Output
 puts ""
