aab4ba48982180bca2ea304fe2268056153f9a42
azazelo <andrey.eremeyev@gmail.com>
2018-12-13 14:50:52 +0400

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,15 @@
+require "colorize"
+
+commands = ("оплпол"*100).split("")
+
+alfabet30 = "АБВГДЕЁЖЗИКЛМНОПРСТУФХЧЦШЩЫЭЮЯ".split("")
+res = alfabet30.map{|l| l + commands[rand(300)].red.italic}
+
+# Output
+puts ""
+puts "+-------------------+"
+res.each_slice(6) do |six|
+  puts "| " + six.join(" ") + " |"
+end
+puts "+-------------------+"
+puts ""
