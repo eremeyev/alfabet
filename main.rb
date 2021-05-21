d07de8519333addafb1b84862756ce053ec93d8c
Andrey Eremeev <andrey.eremeyev@gmail.com>
2021-05-21 13:23:31 +0400

    refactoring

--- main.rb
+++ main.rb
@@ -8,8 +8,8 @@ res = alfabet30.map{|l| l + commands[rand(300)].red}
 # Output
 puts ""
 puts "+-------------------+"
-res.each_slice(6) do |six|
-  puts "| " + six.join(" ") + " |"
+res.each_slice(6) do |row|
+  puts "| " + row.join(" ") + " |"
 end
 puts "+-------------------+"
 puts ""
